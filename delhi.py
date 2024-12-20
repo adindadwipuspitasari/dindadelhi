da630188a3e0e0eaf4eb01acc45a01e663bd65c1
Adinda dwi puspitasari <113650400+adindadwipuspitasari@users.noreply.github.com>
2024-12-20 11:07:19 +0700

    Update delhi.py

--- delhi.py
+++ delhi.py
@@ -48,16 +48,16 @@ try:
 
     # Menampilkan 10 hotel di kolom kanan
     with col1:
-        st.subheader("10 Hotel Ver Good")
-        tidak_populer = df[df['Rating Description'] == "Very Good"].head(10)[['Hotel Name', 'Rating', 'Rating Description']]
+        st.subheader("Hotel dengan rating Very Good")
+        tidak_populer = df[df['Rating Description'] == "Very Good"].head(15)[['Hotel Name', 'Rating', 'Rating Description']]
         tidak_populer = tidak_populer.reset_index(drop=True)  # Reset index, drop kolom index lama
         tidak_populer.index = tidak_populer.index + 1  # Set index mulai dari 1
         st.dataframe(tidak_populer)
 
     # Menampilkan 10 di kolom kiri
     with col2:
-        st.subheader("10 Hotel Exellent")
-        populer = df[df['Rating Description'] == "Excellent"].head(10)[['Hotel Name', 'Rating', 'Rating Description']]
+        st.subheader("Hotel dengan rating Exellent")
+        populer = df[df['Rating Description'] == "Excellent"].head(15)[['Hotel Name', 'Rating', 'Rating Description']]
         populer = populer.reset_index(drop=True)  # Reset index, drop kolom index lama
         populer.index = populer.index + 1  # Set index mulai dari 1
         st.dataframe(populer)
